ae186d3827558a26f13d6c9babd481da34c39169
alanchang15 <alanchang15@gmail.com>
2019-06-11 12:19:19 +0800

    Update compatible.php

--- helpers/compatible.php
+++ helpers/compatible.php
@@ -3,15 +3,13 @@
 if (!function_exists('redirect')) {
     function redirect($uri = '', $method = 'auto', $code = null)
     {
-        $app = \CI\Swoole\Core\Application::forge();
-
         if (!preg_match('#^(\w+:)?//#i', $uri)) {
             $uri = site_url($uri);
         }
 
         $code = 302;
 
-        $app->response->redirect($uri, $code);
+        response()->redirect($uri, $code);
 
         exit;
     }
